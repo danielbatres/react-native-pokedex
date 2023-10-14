39a5d03a482e2f09809d98b41c3942c9e2c492e1
danielbatres <danielbatresuser@gmail.com>
2023-10-13 21:01:54 -0600

    Get information about pokemon

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,8 +1,13 @@
 import { API_HOST } from "../utils/constants";
 
 export async function getPokemonsApi() {
+  const url = `${API_HOST}/pokemon?limit=20&offset=0`;
+
+  return await getPokemonDetailsByUrlApi(url);
+}
+
+export async function getPokemonDetailsByUrlApi(url) {
   try {
-    const url = `${API_HOST}/pokemon?limit=20&offset=0`;
     const response = await fetch(url);
     const result = await response.json();
 
