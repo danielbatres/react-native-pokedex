009a8e045bd43bc330d5333c7e03fc9283530c22
danielbatres <danielbatresuser@gmail.com>
2023-10-13 22:08:40 -0600

    Infinite scrolling with FlatList

--- src/api/pokemon.js
+++ src/api/pokemon.js
@@ -1,9 +1,9 @@
 import { API_HOST } from "../utils/constants";
 
-export async function getPokemonsApi() {
+export async function getPokemonsApi(endpointUrl) {
   const url = `${API_HOST}/pokemon?limit=20&offset=0`;
 
-  return await getPokemonDetailsByUrlApi(url);
+  return await getPokemonDetailsByUrlApi(endpointUrl || url);
 }
 
 export async function getPokemonDetailsByUrlApi(url) {
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,25 +1,44 @@
-import { StyleSheet, Text, FlatList } from 'react-native'
+import { StyleSheet, ActivityIndicator, FlatList } from 'react-native'
 import React from 'react'
 import { PokemonCard } from './PokemonCard';
 
 function PokemonList(props) {
-  const { pokemons } = props;
+  const { pokemons, loadPokemons, isNext } = props;
+
+  const loadMore = () => {
+    loadPokemons();
+  }
 
   return (
-    <FlatList 
+    <FlatList
       data={pokemons}
       numColumns={2}
       showsVerticalScrollIndicator={false}
-      keyExtractor={pokemon => String(pokemon.id)}
+      keyExtractor={(pokemon) => String(pokemon.id)}
       renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
+      onEndReached={isNext && loadMore}
+      onEndReachedThreshold={0.1}
+      ListFooterComponent={
+        isNext && (
+          <ActivityIndicator
+            size="large"
+            style={styles.spinner}
+            color="#AEAEAE"
+          />
+        )
+      }
     />
-  )
+  );
 }
 
 const styles = StyleSheet.create({
   flatListContentContainer: {
     paddingHorizontal: 5
+  },
+  spinner: {
+    marginTop: 20,
+    marginBottom: 60
   }
 })
 
