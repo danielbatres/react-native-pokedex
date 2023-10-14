9c9b0ee9138e8bc54c53cad9507524989416c3e9
danielbatres <danielbatresuser@gmail.com>
2023-10-13 21:32:45 -0600

    Rendering pokemon card

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,5 +1,6 @@
 import { StyleSheet, Text, FlatList } from 'react-native'
 import React from 'react'
+import { PokemonCard } from './PokemonCard';
 
 function PokemonList(props) {
   const { pokemons } = props;
@@ -10,7 +11,7 @@ function PokemonList(props) {
       numColumns={2}
       showsVerticalScrollIndicator={false}
       keyExtractor={pokemon => String(pokemon.id)}
-      renderItem={({ item }) => <Text>{item.name}</Text>}
+      renderItem={({ item }) => <PokemonCard pokemon={item} />}
       contentContainerStyle={styles.flatListContentContainer}
     />
   )
