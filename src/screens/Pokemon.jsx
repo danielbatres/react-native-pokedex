75dd7e047dac613160d29d6a29ddc44d7519a534
danielbatres <danielbatresuser@gmail.com>
2023-10-14 15:37:30 -0600

    Navigation bar layout

--- src/screens/Pokemon.jsx
+++ src/screens/Pokemon.jsx
@@ -1,5 +1,6 @@
 import { ScrollView } from 'react-native'
 import React, { useState, useEffect } from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import { getPokemonDetailsApi } from '../api/pokemon';
 import { Header } from '../components/Pokemon/Header';
 import { Type } from '../components/Pokemon/Type';
@@ -14,6 +15,21 @@ function Pokemon(props) {
   } = props;
 
   const [pokemon, setPokemon] = useState(null);
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => null,
+      headerLeft: () => (
+        <Icon 
+          name='arrow-left' 
+          color='#fff' 
+          size={20}
+          style={{ marginLeft: 20 }}
+          onPress={navigation.goBack}
+        />
+      )
+    });
+  }, [navigation, params]);
 
   useEffect(() => {
     (async () => {
