625a4c5ab7c9b0eca8eb0f7d7f00359495549724
danielbatres <danielbatresuser@gmail.com>
2023-10-13 20:44:28 -0600

    Api consumption with fetch, pokeapi

--- src/screens/Pokedex.jsx
+++ src/screens/Pokedex.jsx
@@ -1,7 +1,23 @@
 import { SafeAreaView, Text } from "react-native";
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { getPokemonsApi } from "../api/pokemon";
 
 function Pokedex() {
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })()
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonsApi();
+      console.log(response);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
