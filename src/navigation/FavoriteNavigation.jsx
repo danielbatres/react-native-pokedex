fc07127d449f21153e174bcf5393838ee29b5b0d
danielbatres <danielbatresuser@gmail.com>
2023-10-15 15:44:43 -0600

    Back navigation in favorites

--- src/navigation/FavoriteNavigation.jsx
+++ src/navigation/FavoriteNavigation.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import { createStackNavigator } from '@react-navigation/stack'
 import { Favorites } from '../screens/Favorites';
+import { Pokemon } from '../screens/Pokemon';
 
 const Stack = createStackNavigator();
 
 function FavoriteNavigation() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name='Favorites' component={Favorites} options={{
-        title: 'Favorites'
-      }} />
+      <Stack.Screen 
+        name='Favorites' 
+        component={Favorites} 
+        options={{
+          title: 'Favorites'
+        }}
+      />
+      <Stack.Screen 
+        name='Pokemon'
+        component={Pokemon}
+        options={{
+          title: '',
+          headerTransparent: true
+        }}
+      />
     </Stack.Navigator>
   )
 }
