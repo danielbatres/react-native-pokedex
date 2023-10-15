9dd86bd5cb6624912a294e582f5d6b26f5b83893
danielbatres <danielbatresuser@gmail.com>
2023-10-15 12:28:36 -0600

    Implementing formik and yup to validate the form

--- src/components/Auth/LoginForm.jsx
+++ src/components/Auth/LoginForm.jsx
@@ -1,24 +1,53 @@
 import { StyleSheet, View, Text, TextInput, Button, Keyboard } from 'react-native'
 import React from 'react'
+import { useFormik } from 'formik'
+import * as Yup from 'yup'
 
 function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validateOnChange: false,
+    validationSchema: Yup.object(validationSchema()),
+    onSubmit: formValue => console.log(formValue)
+  });
+
   return (
     <View>
       <Text style={styles.title}>Login</Text>
-      <TextInput 
-        placeholder='Enter username'
+      <TextInput
+        placeholder="Enter username"
         style={styles.input}
-        autoCapitalize='none'
+        autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue("username", text)}
       />
-      <TextInput 
-        placeholder='Enter password'
+      <TextInput
+        placeholder="Enter password"
         style={styles.input}
-        autoCapitalize='none'
+        autoCapitalize="none"
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
       />
-      <Button title='Login' onPress={() => console.log('login')} />
+      <Button title="Login" onPress={formik.handleSubmit} />
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
-  )
+  );
+}
+
+function initialValues() {
+  return {
+    username: '',
+    password: ''
+  }
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required('Username is required'),
+    password: Yup.string().required('Password is required')
+  }
 }
 
 const styles = StyleSheet.create({
@@ -35,6 +64,11 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     padding: 10,
     borderRadius: 10
+  },
+  error: {
+    textAlign: 'center',
+    color: 'red',
+    marginTop: 20
   }
 })
 
